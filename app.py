7ce327fb97725e4b5d13bb4034c065cdc36ab884
Talita Cavalcanti <107768582+talitecm@users.noreply.github.com>
2024-12-07 18:19:30 +0000

    vs 1

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
+def inicio():
+    return render_template('inicio.html')
+
+@app.route('/pizzaria')
+def pizzaria():
+    return render_template('pizzaria.html')
+
+""" ---------------------------------- """
+
+@app.route('/calc_imc')
 def index():
     return render_template('index.html')
 
@@ -11,5 +21,18 @@ def imc():
     nome = request.args.get("nome")
     altura = float(request.args.get("altura"))
     peso = float(request.args.get("peso"))
-    imc = round((peso/altura),2)
-    return render_template("imc.html", nomeuser = nome, imcuser = imc)
+    imc = round((peso/altura**2),2)
+
+    if (imc < 18.5):
+        mensagem = "Você está abaixo do peso!"
+
+    elif(imc < 24.9):
+        mensagem = "Você está com o peso normal!"
+
+    elif(imc < 29.9):
+        mensagem = "Você está com o sobrepeso!"
+    
+    else:
+        mensagem = "Você está obeso!"
+
+    return render_template("imc.html", mensagem = mensagem)
