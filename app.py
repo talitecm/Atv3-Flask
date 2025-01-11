484d76c3e078cb9c7fe39c90bdad1a96bf0b0da4
Talita Cavalcanti <107768582+talitecm@users.noreply.github.com>
2025-01-11 01:53:40 +0000

    Final

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,15 +10,29 @@ def inicio():
 def pizzaria():
     return render_template('pizzaria.html')
 
-@app.route('/fazer-pedido')
+@app.route('/fazer-pedido', methods=["GET", "POST"])
 def fazer_pedido():
     return render_template('fazer-pedido.html')
+
+@app.route('/confirmacao', methods=["POST"])
+def confirmacao():
+    # Recebe os dados enviados pelo formulário
+    massa = request.form.get("massa")
+    queijo = request.form.get("queijo")
+    recheio = request.form.get("recheio")
+    borda = request.form.get("borda")
+    
+    return render_template("confirmacao.html", 
+                           massa=massa, 
+                           queijo=queijo, 
+                           recheio=recheio, 
+                           borda=borda)
 
 @app.route('/calc_imc')
 def calc_imc():
     return render_template('calc-imc.html')
 
-@app.route('/imc', methods=["GET"])
+@app.route('/imc', methods=["GET", "POST"])
 def imc():
     nome = request.args.get("nome")
     altura = float(request.args.get("altura"))
